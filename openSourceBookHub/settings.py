b3a49d283259d7ef9bef2f8ce60b4dd040807d37
David-Inkheart <okoliedave1@gmail.com>
2023-04-10 18:09:04 +0100

    updated allowed hosts

--- openSourceBookHub/settings.py
+++ openSourceBookHub/settings.py
@@ -43,8 +43,8 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = ['*', 'http://127.0.0.1:8000']
-ALLOWED_HOSTS = os.getenv(
-    "DJANGO_ALLOWED_HOSTS,https://osbh-app-cxg9t.ondigitalocean.app,127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",
+                          "https://osbh-app-cxg9t.ondigitalocean.app")
 CSRF_TRUSTED_ORIGINS = ['https://osbh-app-cxg9t.ondigitalocean.app/']
 
 
