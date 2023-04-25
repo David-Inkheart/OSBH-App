2ede2d0628589b9fa5b3c4ccd90383bd14ca2d7e
David-Inkheart <okoliedave1@gmail.com>
2023-04-25 10:34:24 +0100

    updated allowed host

--- openSourceBookHub/settings.py
+++ openSourceBookHub/settings.py
@@ -44,8 +44,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 # ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = ['*', 'http://127.0.0.1:8000']
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",
-                          "127.0.0.1,localhost",
-                          ".vercel.app").split(",")
+                          "127.0.0.1,localhost,.vercel.app").split(",")
 CSRF_TRUSTED_ORIGINS = ['https://osbh-app-cxg9t.ondigitalocean.app/']
 
 
