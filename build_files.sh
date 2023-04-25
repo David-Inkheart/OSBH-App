8272e2c3d4f2693c3f084d5ce5018433946643d9
David-Inkheart <okoliedave1@gmail.com>
2023-04-25 10:42:38 +0100

    reverted to digitalocean

--- build_files.sh
+++
@@ -1,7 +0,0 @@
-# build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
-python manage.py makemigrations
-python manage.py migrate
-python manage.py makemigrations blog forum file_library
-python manage.py migrate
